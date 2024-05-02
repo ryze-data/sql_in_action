2e5b80a2106e841fabccd474a2da210db410bd7d
Matt Jeffs <m.s.jeffs50@gmail.com>
2024-05-01 22:49:40 -0600

    Update simple_sql.sql

--- simple_sql.sql
+++ simple_sql.sql
@@ -2,4 +2,4 @@
 Testing functionality
 */
 
-select customer_id, fiRSt_name, lasT_name from customers where customer_id = 12;
+select customer_id, fiRSt_nAme, lasT_name from customers where customer_id = 12;
