58749c882e087019afc1dbcddc1afb138aff59d9
Matt Jeffs <m.s.jeffs50@gmail.com>
2024-05-01 23:28:02 -0600

    Update customer.sql

--- tests/customer.sql
+++ tests/customer.sql
@@ -2,4 +2,4 @@
 Testing functionality
 */
 
-select customER_id, fiRSt_nAme, lasT_Name from customers where customer_id = 12;
+select customER_id, firSt_nAme, lasT_Name from customers where customer_id = 12;
